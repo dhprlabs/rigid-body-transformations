e94cb7197c37e324af669653c245835a38b17b58
dhprlabs <dhprlabs@gmail.com>
2025-08-18 21:01:07 +0530

    add wasm build

---
+++ build_wasm/CMakeFiles/Transforms.dir/DependInfo.cmake
@@ -0,0 +1,24 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "C:/Users/Dhairya/Desktop/Local/Github/rigid-body-transformations/Transforms.cpp" "CMakeFiles/Transforms.dir/Transforms.cpp.o" "gcc" "CMakeFiles/Transforms.dir/Transforms.cpp.o.d"
+  "C:/Users/Dhairya/Desktop/Local/Github/rigid-body-transformations/src/RigidBodyTransforms.cpp" "CMakeFiles/Transforms.dir/src/RigidBodyTransforms.cpp.o" "gcc" "CMakeFiles/Transforms.dir/src/RigidBodyTransforms.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build_wasm/CMakeFiles/Transforms.dir/cmake_clean.cmake
@@ -0,0 +1,14 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Transforms.dir/Transforms.cpp.o"
+  "CMakeFiles/Transforms.dir/Transforms.cpp.o.d"
+  "CMakeFiles/Transforms.dir/src/RigidBodyTransforms.cpp.o"
+  "CMakeFiles/Transforms.dir/src/RigidBodyTransforms.cpp.o.d"
+  "Transforms.js"
+  "Transforms.js.manifest"
+  "Transforms.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Transforms.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
